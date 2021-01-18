8909b7225ce3239371c9b5b9d625c162a398dd1b
yuusuke-nozaki <yuusuke_nozaki@sobal.co.jp>
2021-01-18 15:17:24 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { disconnect } from 'process';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <div>App Test</div>
+    </div>
+  );
+}
+
+export default App;
