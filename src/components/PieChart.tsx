4835351a8c61dd39101d6dd7f71c2c0cd564411c
yuusuke-nozaki <yuusuke_nozaki@sobal.co.jp>
2021-01-22 11:59:07 +0900

    TextBox値読み込みapi実装

--- src/components/PieChart.tsx
+++ src/components/PieChart.tsx
@@ -1,16 +1,26 @@
 import { Pie } from 'react-chartjs-2';
 
+// import GetApi from './GetApi';
+
 const PieChart = () => {
 
-    let planNumber: number = 100;
-    let suddenNumber: number = 10;
+    //計画総数
+    let planNumber = function() {
+        // return GetApi("totalplan", "dummy");
+        return 100;
+    };
+    //突発総数
+    let suddenNumber = function() {
+        // return GetApi("totalsudden", "dummy");
+        return 10;
+    };
 
     const data = {
         "labels": ["計画", "突発"],
         "datasets": [{
             "label": "計画突発比率",
             // 値をそのまま入れていい
-            "data": [planNumber, suddenNumber],
+            "data": [planNumber(), suddenNumber()],
             "backgroundColor": ["rgb(255, 99, 132)",
                 "rgb(54, 162, 235)"]
         }]
